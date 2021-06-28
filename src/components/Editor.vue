95d047b079c9779b10a15c9678d7a3c1d1361d12
Xuyang Cao <Xuyang.Cao@microsoft.com>
2021-06-28 19:51:43 +0800

    increase typing speed

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -82,14 +82,14 @@
           let count = 0, typingCount = 0, typing
           // 写代码每一帧的函数
           let step = () => {
-            let randomNumber = Math.round(Math.random() * 6)
+            let randomNumber = Math.round(Math.random() * 12 + 12)
             // 摸你打字的随机速度
             if(count % 2 === 0 && randomNumber % 4 === 0){
               this.currentCode = this.code.substring(0, typingCount)
               typingCount++
             }
             // 大约每 24 帧光标闪动一次
-            if(count % 24 === 0){
+            if(count % 10 === 0){
               this.isCursorVisible = this.isCursorVisible === 0 ? 1 : 0
             }
             count++
